c666494d433020b7b427cdb980c105f944a46478
Britta <britta@AdminNUSs-MacBook-Pro-7.local>
2025-10-14 14:49:15 +0530

    added the model

--- src/kyc_pipeline/router/router.py
+++ src/kyc_pipeline/router/router.py
@@ -25,6 +25,7 @@ def llmrouter(model_name: str = "gpt-5-nano", temperature: float = 0.05) -> LLM:
         - If any error occurs, fall back to gpt-4.1-mini"
     """
     
+    model_name="gpt-4.1-mini"
     fallback_model_name="gpt-4.1-mini"
    
     try:
